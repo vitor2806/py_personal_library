531b571958db38744f0be2003a3bdf12cfb063e4
Vitor Rafael <vitor.45678.rafael@gmail.com>
2022-04-21 10:55:10 -0300

    add lambda file

---
+++ lambda.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+def double(number):
+    return number*2
+
+arg = 2
+print(double(arg))
+
+arg = [1, 2, 3, 4, 5]
+print(double(arg)) # when i do list * 2 its only concatenated
+
+print((map(double, arg))) # when i print a map its only returned it address
+double_value = (map(lambda i: i*2, arg)) # lambda is a function that will only be used at once.
+
+# turn map object into an array list then print it 
+double_value = list(double_value)
+print(double_value)
+
+for element in double_value: # to see each element mapped i need to store it on a variable then print it or do as previous
+    print(element)
+
+arg2 = list(filter(lambda i: i%2 == 0, arg))
+print(arg2)
